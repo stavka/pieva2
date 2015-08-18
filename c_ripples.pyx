2143a9c7d495d91d5a3fbbbfd349af7e7b7c508d
Ievgen Varavva <yvaravva@yvaravva-macbookpro.roam.corp.google.com>
2015-08-18 07:55:34 -0700

    Inline more cython code.

--- c_ripples.pyx
+++ c_ripples.pyx
@@ -22,41 +22,44 @@ class Benchmark(object):
 
 @cython.cdivision(True) # turn off /0 check.
 @cython.boundscheck(False) # turn of bounds-checking for entire function
-cdef inline np.ndarray[np.double_t, ndim=2] func_(int t, int sizex, int sizey, double centerx, double centery, double scalex, double scaley, double power, double speed):
-    cdef np.ndarray[np.double_t, ndim=2] res = np.zeros([sizex, sizey], dtype=np.double)
+cdef func_(np.ndarray[np.double_t, ndim=2] res, int t, int sizex, int sizey, double centerx, double centery, double scalex, double scaley, double power, double speed):
 
     cdef int x, y
 
+
+    cdef double i, j
+    cdef double a = t*speed
+
     for x in range(sizex):
         for y in range(sizey):
+
+            i = ((x-centerx)/scalex)
+            j = ((y-centery)/scaley)
             res[x, y] = \
                     sin(( 
-                            ((x-centerx)/scalex) ** 2
+                            i*i
                             +
-                            ((y-centery)/scaley) ** 2
+                            j*j
                         ) ** power
-                    - t * speed) + 1
-    return res
+                    - a) + 1
 
 
 @cython.cdivision(True) # turn off /0 check.
 @cython.boundscheck(False) # turn of bounds-checking for entire function
-cdef inline np.ndarray[np.double_t, ndim=2] make_mask_(int sizex, int sizey, int t, double centerx, double centery, double scalex, double scaley, double start_time, double speed):
-    cdef np.ndarray[np.double_t, ndim=2] res = np.zeros([sizex, sizey], dtype=np.double)
+cdef inline  make_mask_(np.ndarray[np.double_t, ndim=2] res, int sizex, int sizey, int t, double centerx, double centery, double scalex, double scaley, double start_time, double speed):
 
     cdef double age = t - start_time
-
     cdef double sigm = (age + 5) * speed
+    cdef double a = -4*log(2) / sigm**2
 
-    cdef double xx, yy
     cdef int x, y
+    cdef double xx, yy
     for x in range(sizex):
         for y in range(sizey):
             xx = (x-centerx) / scalex;
             yy = (y-centery) / scaley;
 
-            res[x, y] = exp(-4*log(2) * ((xx)**2 + (yy)**2) / sigm**2)
-    return res
+            res[x, y] = exp(( xx**2 + yy**2 ) * a)
 
 
 cdef np.ndarray[np.double_t, ndim=3] _render_config(int sizex, int sizey, int t, double centerx, double centery, double scalex, double scaley, double power,
@@ -71,22 +74,30 @@ cdef np.ndarray[np.double_t, ndim=3] _render_config(int sizex, int sizey, int t,
     """
     cdef np.ndarray[np.double_t, ndim=1] bins
     cdef np.ndarray[np.double_t, ndim=2] res, mask
+    cdef np.ndarray[np.uint8_t, ndim=2] r, g, b
     cdef np.ndarray[np.double_t, ndim=3] rgba
 
     with Benchmark("_render"):
+        res = np.zeros([sizex, sizey], dtype=np.double)
         with Benchmark("func_"):
-            res = func_(t, sizex, sizey, centerx, centery, scalex, scaley, power, speed) + 1
+            func_(res, t, sizex, sizey, centerx, centery, scalex, scaley, power, speed)
 
         #with Benchmark("0"):
             bins = np.linspace(np.min(res), np.max(res), len(palette))
 
         with Benchmark("digitize"):
+            print 'palette len', len(palette)
             palette_idxs = np.digitize(res.flatten(), bins[:-1])
 
         with Benchmark("hui"):
-            r, g, b = to_rgb(palette[palette_idxs].reshape((<object>res).shape))
+            r = np.zeros([sizex, sizey], dtype=np.uint8)
+            g = np.zeros([sizex, sizey], dtype=np.uint8)
+            b = np.zeros([sizex, sizey], dtype=np.uint8)
+
+            to_rgb(r, g, b, palette[palette_idxs].reshape((<object>res).shape))
         with Benchmark("hui2"):
-            mask = make_mask_(sizex, sizey, t, centerx, centery, scalex, scaley, start_time, speed)
+            mask = np.zeros([sizex, sizey], dtype=np.double)
+            make_mask_(mask, sizex, sizey, t, centerx, centery, scalex, scaley, start_time, speed)
             rgba = np.dstack((r, g, b, mask))
 
     return rgba
@@ -101,9 +112,17 @@ def render_config(sizex, sizey, t, config):
             config['scalex'], config['scaley'], config['power'],
             config['speed'], config['start_time'], config['palette'])
 
-def to_rgb(color):
+@cython.cdivision(True) # turn off /0 check.
+@cython.boundscheck(False) # turn of bounds-checking for entire function
+cdef to_rgb(np.ndarray[np.uint8_t, ndim=2] r, np.ndarray[np.uint8_t, ndim=2] g,
+            np.ndarray[np.uint8_t, ndim=2] b, np.ndarray[np.uint32_t, ndim=2] color):
     """ 0xffffff -> (255, 255, 255)."""
-    b = (color & 255).astype(np.uint8)
-    g = ((color >> 8) & 255).astype(np.uint8)
-    r = ((color >> 16) & 255).astype(np.uint8)
-    return (r, g, b)
+    cdef int sizex = (<object>color).shape[0]
+    cdef int sizey = (<object>color).shape[1]
+
+    cdef int x, y
+    for x in range(sizex):
+        for y in range(sizey):
+            b[x, y] = (color[x,y] & 255)
+            g[x, y] = ((color[x,y] >> 8) & 255)
+            r[x, y] = ((color[x,y] >> 16) & 255)
