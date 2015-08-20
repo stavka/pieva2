fd400f28f0be5f4b2d2296716f277d8bd0f14388
Anton Tolchanov <me@knyar.net>
2015-08-20 01:03:47 +0100

    Add spectrum analyzer.
    - pbdriver.py includes a number of BiblioPixel drivers for Pieva;
    - recorder.py is a pyaudio-based spectrum analyzer BiblioPixel animation
      stolen from github.com/ManiacalLabs/BiblioPixelAnimations;
    - go-spectrum.py actually runs the thing.
    
    Not tested on a Pi yet.

---
+++ go-spectrum.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import argparse
+
+from bibliopixel import LEDMatrix
+from bibliopixel.led import *
+
+from recorder import Recorder, EQ, BassPulse
+from pbdriver import DriverPieva, DriverPievaX4, DriverPievaX4Rev
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Spectrum analyzer')
+    parser.add_argument('--display', choices=['full', 'x4', 'x4rev'], default='x4')
+    parser.add_argument('--anim', choices=['eq', 'bass'], default='eq')
+    parser.add_argument('--fps', default=20)
+    parser.add_argument('--min_freq', default=50)    # 50 Hz
+    parser.add_argument('--max_freq', default=15000) # 15000 Hz
+    args = parser.parse_args()
+
+    if args.display == 'full':
+        w = 140
+        h = 140
+        driver = DriverPieva(width=w, height=h)
+    else:
+        w = 70
+        h = 70
+        if args.display == 'x4':
+            driver = DriverPievaX4(width=w, height=h)
+        else:
+            driver = DriverPievaX4Rev(width=w, height=h)
+
+    led = LEDMatrix(driver, width=w, height=h, serpentine=False)
+    led.setMasterBrightness(255)
+
+    if args.anim == 'eq':
+        anim = EQ(led, args.min_freq, args.max_freq)
+    else:
+        anim = BassPulse(led, args.min_freq, args.max_freq)
+
+    print driver, led, anim
+    try:
+        anim.run(fps=args.fps)
+    except KeyboardInterrupt:
+        pass
+
+    anim.endRecord()
+    led.all_off()
+    led.update()
---
+++ pbdriver.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+from pieva import *
+from screen import Screen
+import numpy as np
+
+from bibliopixel.drivers.driver_base import DriverBase
+
+class DriverPieva(DriverBase):
+    def __init__(self, num=0, width=0, height=0):
+        super(DriverPieva, self).__init__(num, width, height)
+        self.screen = Screen(sections)
+        self.screen.dimm(0)
+        self.pixels = width * height
+        self.width = width
+        self.height = height
+
+    def update(self, data):
+        if len(data) != self.pixels * 3:
+            print "Data length mismatch"
+            return
+
+        bitmap = np.zeros([140, 140])
+        for i in range(self.pixels):
+            r = data[i * 3 + 0]
+            g = data[i * 3 + 1]
+            b = data[i * 3 + 2]
+            rgb = (r << 16) + (g << 8) + b
+            self.draw(bitmap, i, rgb)
+        self.screen.send(bitmap)
+
+    def draw(self, bitmap, i, rgb):
+        bitmap[i % self.width][i / self.height] = rgb
+
+class DriverPievaX4(DriverPieva):
+    def draw(self, bitmap, i, rgb):
+        bitmap[i % self.width][i / self.height] = rgb
+        bitmap[i / self.height][139 - (i % self.width)] = rgb
+        bitmap[139 - (i / self.height)][i % self.width] = rgb
+        bitmap[139 - (i % self.width)][139 - (i / self.height)] = rgb
+
+class DriverPievaX4Rev(DriverPieva):
+    def draw(self, bitmap, i, rgb):
+        raise("Not implemented")
